50e1cae262f34c21dec570a33312eb59e66bbaa4
ScuffedNewt <dragonminer101@gmail.com>
2020-11-19 17:14:59 +0000

    no message

--- resources/views/paypal/paypal_welcome.blade.php
+++ resources/views/paypal/paypal_welcome.blade.php
@@ -25,9 +25,9 @@
     @else <div class="col-md-3 col-6 profile-inventory-item">
     @endif
       {!! Form::open(['action' => 'PaypalController@expressCheckout']) !!}
-        <div class="card p-3" style="height: 385px">
+        <div class="card p-3 mb-1" style="height: 385px">
             <div class="text-center"><h3><strong>{!! $product->item->displayname !!}</strong></h3></div>
-            @if($product->item->category !== Null ) <div class="text-muted"><small>(<a href="{{ $product->item->category->url }}">{!! $product->item->category->name !!})</small></div>@endif
+            @if($product->item->category !== Null ) <h6><div class="text-muted text-center"><a href="{{ $product->item->category->url }}">{!! $product->item->category->name !!}</a></div></h6>@endif
                     <div class="text-center inventory-character">
                         <div class="mb-1">
                             <img style="width: 50%;" src="{{ $product->item->imageurl }}">
