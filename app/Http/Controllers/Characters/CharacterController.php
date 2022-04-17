c6ba7fd77b2a5fe4469611ac30cb1c07760599b9
odius-hades <flgraveson@gmail.com>
2022-04-17 14:27:46 -0400

    Delete duplicate functions

--- app/Http/Controllers/Characters/CharacterController.php
+++ app/Http/Controllers/Characters/CharacterController.php
@@ -403,24 +403,6 @@ class CharacterController extends Controller
     }
 
     /**
-     * Transfers inventory items back to a user.
-     *
-     * @param  \Illuminate\Http\Request       $request
-     * @param  App\Services\InventoryManager  $service
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    private function postItemTransfer(Request $request, InventoryManager $service)
-    {
-        if($service->transferCharacterStack($this->character, $this->character->user, CharacterItem::find($request->get('ids')), $request->get('quantities'))) {
-            flash('Item transferred successfully.')->success();
-        }
-        else {
-            foreach($service->errors()->getMessages()['error'] as $error) flash($error)->error();
-        }
-        return redirect()->back();
-    }
-
-    /**
      * Transfers inventory awards back to a user.
      *
      * @param  \Illuminate\Http\Request       $request
@@ -438,41 +420,6 @@ class CharacterController extends Controller
         return redirect()->back();
     }
 
-    /**
-     * Names an inventory stack.
-     *
-     * @param  \Illuminate\Http\Request       $request
-     * @param  App\Services\CharacterManager  $service
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    private function postName(Request $request, InventoryManager $service)
-    {
-        if($service->nameStack($this->character, CharacterItem::find($request->get('ids')), $request->get('stack_name'))) {
-            flash('Item named successfully.')->success();
-        }
-        else {
-            foreach($service->errors()->getMessages()['error'] as $error) flash($error)->error();
-        }
-        return redirect()->back();
-    }
-
-    /**
-     * Deletes an inventory stack.
-     *
-     * @param  \Illuminate\Http\Request       $request
-     * @param  App\Services\CharacterManager  $service
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    private function postDelete(Request $request, InventoryManager $service)
-    {
-        if($service->deleteStack($this->character, CharacterItem::find($request->get('ids')), $request->get('quantities'))) {
-            flash('Item deleted successfully.')->success();
-        }
-        else {
-            foreach($service->errors()->getMessages()['error'] as $error) flash($error)->error();
-        }
-        return redirect()->back();
-    }
     /**
      * Deletes an award stack.
      *
