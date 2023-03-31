d1a875321580901b5a577895b2ba52f8fb31698e
JAM\mallo <mallowmagicalus@Gmail.com>
2023-03-31 13:16:00 -0400

    no message

--- resources/views/admin/emotes/emotes.blade.php
+++ resources/views/admin/emotes/emotes.blade.php
@@ -7,7 +7,7 @@
 
 <h1>Emotes</h1>
 
-<p>This is a list of emotes players/admins can use in the tinymce editor.</p>
+<p>This is a list of emotes players/admins can use in the tinymce editor (and in comments).</p>
 
 <div class="text-right mb-3">
     <a class="btn btn-primary" href="{{ url('admin/emotes/create') }}"><i class="fas fa-plus"></i> Create New Emote</a>
@@ -20,6 +20,7 @@
         <thead>
             <tr>
                 <th>Name</th>
+                <th>Is Visible</th>
                 <th></th>
             </tr>
         </thead>
@@ -29,6 +30,7 @@
                     <td>
                     {!! $emote->imageUrl ? '<img src="'. $emote->imageUrl .'" class="img-fluid mr-2" style="height: 2em;" />' : ''!!} {{ $emote->name }}</a>
                     </td>
+                    <td>{{$emote->is_active ? 'Active' : ' '}}</td>
                     <td class="text-right">
                         <a href="{{ url('admin/emotes/edit/'.$emote->id) }}" class="btn btn-primary">Edit</a>
                     </td>
