20f10f12c4b0a32938caba153cee24d9a74e7478
asteriddle <97659407+asteriddle@users.noreply.github.com>
2023-06-27 13:59:12 -0600

    remove test message and add bg that i can actually use

--- resources/views/pages/_dashboard.blade.php
+++ resources/views/pages/_dashboard.blade.php
@@ -11,7 +11,6 @@
 <div class="temp">
 
 <div class="row">
-    <p>TEXTING TESTING 123</P>
     <div class="col-md-6">
         <div class="card mb-4">
             <div class="card-body text-center">
