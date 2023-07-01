51aebc115c14776d61daf9fe26db3492aa795201
asteriddle <97659407+asteriddle@users.noreply.github.com>
2023-06-30 21:14:54 -0600

    test fix

--- resources/views/character/_image_js.blade.php
+++ resources/views/character/_image_js.blade.php
@@ -40,6 +40,29 @@
             e.preventDefault();
             loadModal("{{ url('admin/'.($character->is_myo_slot ? 'myo' : 'character').'/') }}/{{ $character->is_myo_slot ? $character->id : $character->slug }}/genome/"+$(this).data('genome-id')+"/delete", 'Delete Genome');
         });
-        
+        $('.edit-notes').on('click', function(e) {
+            e.preventDefault();
+            $("div.imagenoteseditingparse").load("{{ url('admin/character/image') }}/" + $(this).data('id') + "/notes", function() {
+                tinymce.init({
+                    selector: '.imagenoteseditingparse .wysiwyg',
+                    height: 500,
+                    menubar: false,
+                    convert_urls: false,
+                    plugins: [
+                        'advlist autolink lists link image charmap print preview anchor',
+                        'searchreplace visualblocks code fullscreen spoiler',
+                        'insertdatetime media table paste code help wordcount'
+                    ],
+                    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | spoiler-add spoiler-remove | removeformat | code',
+                    content_css: [
+                        '{{ asset('css/app.css') }}',
+                        '{{ asset('css/lorekeeper.css') }}'
+                    ],
+                    spoiler_caption: 'Toggle Spoiler',
+                    target_list: false
+                });
+            });
+            $(".edit-notes").remove();
+        });
         
 </script>
