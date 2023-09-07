eb3f1d183e3d37063c229f3d788fd55352c6a2d7
asteriddle <97659407+asteriddle@users.noreply.github.com>
2023-09-07 09:47:15 -0600

    peas.

--- database/migrations/2023_09_07_114250_please_add_in_breeding_permissions_thanks.php
+++ database/migrations/2023_09_07_114250_please_add_in_breeding_permissions_thanks.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CREATEDABREEDINGPERMTABLESPLEASE extends Migration
+class CREATEDABREEDINGPERMSSPLEASE extends Migration
 {
     /**
      * Run the migrations.
