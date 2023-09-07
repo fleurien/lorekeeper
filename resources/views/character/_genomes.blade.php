fbe079b7827ce058c3304e09c6c9bc2c95acaa16
asteriddle <97659407+asteriddle@users.noreply.github.com>
2023-09-07 10:34:31 -0600

    re add edit genes button and also fix char icon size

--- resources/views/character/_genomes.blade.php
+++ resources/views/character/_genomes.blade.php
@@ -1,3 +1,17 @@
+@php
+    $button = "";
+    if (Auth::user() && Auth::user()->hasPower('manage_characters') && Auth::user()->hasPower('view_hidden_genetics')) {
+        $button .= "<a href=\"#\" class=\"btn btn-link btn-sm ";
+        if($genome)
+            $button .= "edit-genome\" data-genome-id=\"". $genome->id ."\"><i class=\"fas fa-cog\"";
+        else
+            $button .= "add-genome\"><i class=\"fas fa-plus\"";
+
+        $button .= "></i></a>";
+        if($genome) $button .= "<a href=\"#\" class=\"btn btn-link btn-sm text-danger delete-genome\" data-genome-id=\"". $genome->id ."\"><i class=\"fas fa-minus\"></i></a>";
+    }
+@endphp
+
 @if ($character->genomes->count())
     @foreach ($character->genomes as $genome)
         @include('character._genes', ['genome' => $genome])
--- resources/views/user/_profile_content.blade.php
+++ resources/views/user/_profile_content.blade.php
@@ -79,7 +79,7 @@
         @foreach ($chunk as $character)
             <div class="col-md-3 col-6 text-center">
                 <div>
-                    <a href="{{ $character->url }}"><img src="{{ $character->image->thumbnailUrl }}" class="img-thumbnail" alt="{{ $character->fullName }}" /></a>
+                    <a href="{{ $character->url }}"><img src="{{ $character->image->thumbnailUrl }}" class="img-thumbnail" alt="{{ $character->fullName }}" style="max-width:200px;max-height:200px;" /></a>
                 </div>
                 <div class="mt-1">
                     <a href="{{ $character->url }}" class="h5 mb-0">
