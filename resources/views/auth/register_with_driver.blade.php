e759a28ea31f2987088cf102522f234a06e1e592
asteriddle <97659407+asteriddle@users.noreply.github.com>
2023-09-08 17:07:18 -0600

    hotfix to add captcha to the driver reg (oops)

--- resources/views/auth/register_with_driver.blade.php
+++ resources/views/auth/register_with_driver.blade.php
@@ -71,6 +71,8 @@
             </div>
         </div>
 
+        {!! RecaptchaV3::field('register') !!}
+
         <div class="form-group row mb-0">
             <div class="col-md-6 offset-md-4">
                 <button type="submit" class="btn btn-primary">
