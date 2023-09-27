a66f7713a56541a14f474adc445b24709772ac6d
asteriddle <97659407+asteriddle@users.noreply.github.com>
2023-09-27 11:00:34 -0600

    awardcase fix(?) and css fixes

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-expand-md navbar-light">
-    <ul class="navbar-nav ml-auto mr-auto">
+<nav class="navbar justify-content-center navbar-expand-md navbar-light">
+    <ul class="navbar-nav ml-auto mr-auto justify-content-center">
         <li class="nav-item"><a href="{{ url('info/about') }}" class="nav-link">About</a></li>
         <li class="nav-item"><a href="{{ url('info/terms') }}" class="nav-link">Terms</a></li>
         <li class="nav-item"><a href="{{ url('info/privacy') }}" class="nav-link">Privacy</a></li>
